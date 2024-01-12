178b70659c4fb7036177ee199effef8f1d7568ed
Aeldit <raphael.roger.92@gmail.com>
2024-01-12 10:10:55 +0100

    First commit

---
+++ CTM Logs.py
@@ -0,0 +1,84 @@
+from PIL import Image
+
+
+def get_pixel(color: tuple[int, int, int], local_img):
+    if color == (184, 148, 95, 255):
+        return local_img.getpixel((1, 1))
+    elif color == (126, 98, 55, 255):
+        return local_img.getpixel((2, 2))
+    elif color == (150, 116, 65, 255):
+        return local_img.getpixel((4, 2))
+    elif color == (159, 132, 77, 255):
+        return local_img.getpixel((6, 2))
+    elif color == (194, 157, 98, 255):
+        return local_img.getpixel((3, 3))
+    elif color == (175, 143, 85, 255):
+        return local_img.getpixel((5, 1))
+    else:
+        return color
+
+
+def set_borders(index: int, ref_img: Image, new_img: Image):
+    if index in [1, 2, 3, 4, 5, 7, 12, 13, 14, 15, 29, 31]:
+        for i in range(16):
+            new_img.putpixel((i, 0), ref_img.getpixel((i, 0)))
+
+    if index in [1, 2, 3, 16, 17, 18, 36, 37, 38, 39, 40, 42]:
+        for i in range(16):
+            new_img.putpixel((i, 15), ref_img.getpixel((i, 15)))
+
+    if index in [1, 4, 6, 12, 13, 16, 24, 25, 28, 30, 36, 37]:
+        for i in range(16):
+            new_img.putpixel((0, i), ref_img.getpixel((0, i)))
+
+    if index in [3, 5, 12, 15, 17, 19, 24, 27, 36, 39, 41, 43]:
+        for i in range(16):
+            new_img.putpixel((15, i), ref_img.getpixel((15, i)))
+
+    # Corners
+    if index in [8, 9, 17, 18, 19, 21, 22, 23, 34, 40, 43, 45, 46]:
+        for i in range(16):
+            new_img.putpixel((0, 0), ref_img.getpixel((0, 0)))
+
+    if index in [6, 9, 10, 16, 18, 20, 21, 22, 28, 35, 42, 44, 46]:
+        for i in range(16):
+            new_img.putpixel((15, 0), ref_img.getpixel((15, 0)))
+
+    if index in [5, 7, 8, 9, 11, 19, 20, 23, 31, 33, 35, 41, 46]:
+        for i in range(16):
+            new_img.putpixel((0, 15), ref_img.getpixel((0, 15)))
+
+    if index in [4, 6, 7, 8, 10, 11, 20, 21, 29, 30, 32, 34, 46]:
+        for i in range(16):
+            new_img.putpixel((15, 15), ref_img.getpixel((15, 15)))
+    return None
+
+
+def main():
+    general_path = "C:/Users/rapha/AppData/Roaming/PrismLauncher/instances/1.20/.minecraft/resourcepacks/CTM OF Fabric/assets/minecraft/optifine/ctm/connect/organics/"
+
+    ref_dir = "oak/"
+    # ref_dir = "stripped_oak/"
+
+    log_types = ["acacia", "birch", "cherry", "crimson", "dark_oak", "jungle", "mangrove", "spruce", "warped"]
+
+    for a in range(len(log_types)):
+        log_types.append("stripped_" + log_types[a])
+
+    for log_type in log_types:
+        for i in range(1, 47):
+            done_image = Image.open(general_path + ref_dir + str(i) + ".png")
+            local_img = Image.open(general_path + log_type + "/" + "0.png")
+            new_img = Image.new("RGBA", (16, 16))
+
+            for line in range(16):
+                for column in range(16):
+                    new_img.putpixel((line, column), get_pixel(done_image.getpixel((line, column)), local_img))
+                    set_borders(i, local_img, new_img)
+
+            new_img.save(general_path + log_type + "/" + str(i) + ".png")
+            new_img.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ DS-GUI hover replacement.py
@@ -0,0 +1,73 @@
+from PIL import Image
+
+
+def main(replacements: list[tuple[int, int, int, int]], path: str, replace_positions: list[tuple[int, int]]):
+    image = Image.open(path)
+    size = image.size
+    new_img = Image.new("RGBA", size)
+    replaced = False
+    colors = [image.getpixel(replace_positions[j]) for j in range(len(replace_positions))]
+
+    for line in range(size[0]):
+        for column in range(size[1]):
+            for i in range(len(replacements)):
+                if image.getpixel((line, column)) == colors[i] and not replaced:
+                    new_img.putpixel((line, column), replacements[i])
+                    replaced = True
+            if not replaced:
+                new_img.putpixel((line, column), image.getpixel((line, column)))
+            else:
+                replaced = False
+
+    new_img.save(path)
+    new_img.close()
+    return 0
+
+
+if __name__ == "__main__":
+    r = [(69, 0, 0, 255), (129, 0, 0, 255)]
+    rp_dir = ("C:/Users/rapha/AppData/Roaming/PrismLauncher/instances/1.20.2/.minecraft/"
+              "resourcepacks/Dark Smooth GUI/assets/")
+
+    # Widgets
+    """minecraft/textures/gui/sprites/widget/button_highlighted.png",
+    "minecraft/textures/gui/sprites/widget/checkbox_highlighted.png",
+    "minecraft/textures/gui/sprites/widget/checkbox_selected_highlighted.png",
+    "minecraft/textures/gui/sprites/widget/slider_handle_highlighted.png",
+    "minecraft/textures/gui/sprites/widget/slider_highlighted.png",
+    "minecraft/textures/gui/sprites/widget/unlocked_button_highlighted.png",
+    # Enchanting table
+    "minecraft/textures/gui/sprites/container/enchanting_table/enchantment_slot_highlighted.png",
+    # Beacon
+    "minecraft/textures/gui/sprites/container/beacon/button_selected.png"""
+    vanilla_paths = [
+
+        # Social interactions
+        "minecraft/textures/gui/sprites/social_interactions/mute_button_highlighted.png",
+        "minecraft/textures/gui/sprites/social_interactions/report_button_highlighted.png",
+        "minecraft/textures/gui/sprites/social_interactions/unmute_button_highlighted.png"
+    ]
+
+    ctms_paths = [
+        "ctms/textures/gui/controls.png",
+        "ctms/textures/gui/reload.png"
+    ]
+
+    iris_path = [
+        "iris/textures/gui/widgets.png"
+    ]
+
+    modmenu_paths = [
+        "modmenu/textures/gui/configure_button.png",
+        "modmenu/textures/gui/filters_button.png",
+        "modmenu/textures/gui/mods_button.png"
+    ]
+
+    etf_paths = [
+        "entity_texture_features/textures/gui/settings_focused.png"
+    ]
+
+    paths = vanilla_paths
+
+    for a in range(len(paths)):
+        main(r, rp_dir + paths[a], [(2, 2), (1, 1)])
