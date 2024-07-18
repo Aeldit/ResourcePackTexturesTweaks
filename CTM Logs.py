5dd48fc9129c61651045d0853a80cbcbab1d8689
aeldit <raphael.roger.92@gmail.com>
2024-07-18 20:57:11 +0200

    Use tuples to speed up things + close images after they are no longer used

--- CTM Logs.py
+++ CTM Logs.py
@@ -32,36 +32,36 @@ def get_pixel(color: tuple[int, int, int], local_img: Image) -> tuple[int, int, 
 
 
 def set_borders(index: int, ref_img: Image, new_img: Image) -> None:
-    if index in [1, 2, 3, 4, 5, 7, 12, 13, 14, 15, 29, 31]:
+    if index in (1, 2, 3, 4, 5, 7, 12, 13, 14, 15, 29, 31):
         for i in range(16):
             new_img.putpixel((i, 0), ref_img.getpixel((i, 0)))
 
-    if index in [1, 2, 3, 16, 17, 18, 36, 37, 38, 39, 40, 42]:
+    if index in (1, 2, 3, 16, 17, 18, 36, 37, 38, 39, 40, 42):
         for i in range(16):
             new_img.putpixel((i, 15), ref_img.getpixel((i, 15)))
 
-    if index in [1, 4, 6, 12, 13, 16, 24, 25, 28, 30, 36, 37]:
+    if index in (1, 4, 6, 12, 13, 16, 24, 25, 28, 30, 36, 37):
         for i in range(16):
             new_img.putpixel((0, i), ref_img.getpixel((0, i)))
 
-    if index in [3, 5, 12, 15, 17, 19, 24, 27, 36, 39, 41, 43]:
+    if index in (3, 5, 12, 15, 17, 19, 24, 27, 36, 39, 41, 43):
         for i in range(16):
             new_img.putpixel((15, i), ref_img.getpixel((15, i)))
 
     # Corners
-    if index in [8, 9, 17, 18, 19, 21, 22, 23, 34, 40, 43, 45, 46]:
+    if index in (8, 9, 17, 18, 19, 21, 22, 23, 34, 40, 43, 45, 46):
         for i in range(16):
             new_img.putpixel((0, 0), ref_img.getpixel((0, 0)))
 
-    if index in [6, 9, 10, 16, 18, 20, 21, 22, 28, 35, 42, 44, 46]:
+    if index in (6, 9, 10, 16, 18, 20, 21, 22, 28, 35, 42, 44, 46):
         for i in range(16):
             new_img.putpixel((15, 0), ref_img.getpixel((15, 0)))
 
-    if index in [5, 7, 8, 9, 11, 19, 20, 23, 31, 33, 35, 41, 46]:
+    if index in (5, 7, 8, 9, 11, 19, 20, 23, 31, 33, 35, 41, 46):
         for i in range(16):
             new_img.putpixel((0, 15), ref_img.getpixel((0, 15)))
 
-    if index in [4, 6, 7, 8, 10, 11, 20, 21, 29, 30, 32, 34, 46]:
+    if index in (4, 6, 7, 8, 10, 11, 20, 21, 29, 30, 32, 34, 46):
         for i in range(16):
             new_img.putpixel((15, 15), ref_img.getpixel((15, 15)))
     return None
@@ -71,7 +71,7 @@ def main() -> None:
     """
     To use the program, we have to put the basic file for each log type (the texture that will be 0.png)
     The images MUST be in RGB or RGBA mode, otherwise the outputs will be fully transparent or black
-    :return: None
+    @return: None
     """
     # Replace this with "absolute/path/to/the/pack/assets
     path_to_rp = "../../Dev/MC-Resource-Packs/CTM_OF_Fabric/assets"
@@ -82,11 +82,12 @@ def main() -> None:
     ref_dir = "oak"
 
     # Obtains the files that are in png format
-    only_files = [f for f in listdir(general_path) if isfile(join(general_path, f))]
-    log_types = []
-    for file in only_files:
-        if file.endswith(".png"):
-            log_types.append(file.removesuffix(".png"))
+    log_types = tuple(
+        file.removesuffix(".png") for file in tuple(
+            f for f in listdir(general_path) if isfile(join(general_path, f))
+        )
+        if file.endswith(".png")
+    )
 
     # Creates a directory for each png file found
     # and moves the associated file inside the created dir (also renames the file to 0.png)
@@ -99,15 +100,19 @@ def main() -> None:
     # Creates the .properties file that will contain the ctm information
     only_dirs = [d for d in listdir(general_path) if isdir(join(general_path, d))]
     if ref_dir not in only_dirs:  # if the dir with the pattern is not present, we can't complete the program
-        return
+        return None
     only_dirs.remove(ref_dir)
+
     for directory in only_dirs:
-        f = open("%s/%s.properties" % (general_path + "/" + directory, directory), "w")
-        f.write(
-            "matchBlocks=%s:%s\nmethod=ctm\ntiles=0-46\nfaces=top bottom\nconnect=tile"
-            % (modid, directory.removesuffix("_top"))
-        )
-        f.close()
+        with open("%s/%s/%s.properties" % (general_path, directory, directory), "w") as wf:
+            wf.write(
+                "matchBlocks=%s:%s\n"
+                "method=ctm\n"
+                "tiles=0-46\n"
+                "faces=top bottom\n"
+                "connect=tile"
+                % (modid, directory.removesuffix("_top"))
+            )
 
     for log_type in log_types:
         for i in range(1, 47):
@@ -122,8 +127,10 @@ def main() -> None:
                     new_img.putpixel((line, column), get_pixel(done_image.getpixel((line, column)), local_img))
                     set_borders(i, local_img, new_img)
 
-            new_img.save(general_path + log_type + "/" + str(i) + ".png")
+            new_img.save("%s%s/%d%s" % (general_path, log_type, i, ".png"))
             new_img.close()
+            local_img.close()
+            done_image.close()
     return None
 
 
